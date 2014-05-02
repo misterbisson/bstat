9b2b64f44f1b63b0cf4fe77e74ee709ca03c29c7
okredo <oren.kredo@gigaom.com>
2014-05-02 16:18:24 -0700

    remove this method

--- components/js/bstat.js
+++ components/js/bstat.js
@@ -294,6 +294,7 @@
 	//
 	// a/b testing framework
 	//
+
 	// set up the event object that will hold bound functions
 	bstat.event = {};
 
@@ -430,20 +431,6 @@
 			'key'  : random_key
 		};
 	};
-
-	/**
-	 * same implementation as used in go-remote-identity-goidentity.read_cookie
-	 */
-	function readCookie( name ) {
-		var nameEQ = name + "=";
-		var ca = document.cookie.split(';');
-		for(var i=0;i < ca.length;i++) {
-			var c = ca[i];
-			while (c.charAt(0)==' ') c = c.substring(1,c.length);
-			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-		}
-		return null;
-	}
 
 	// install an a/b test variation, if any
 	bstat.init();
